a7166c7205b4dc78ded9d8d0bbfa43a39329f348
Alex Jackson <ajjackson91195@gmail.com>
2018-06-10 20:54:56 -0700

    Finish move generation for chess engine (with some unfinished logic, i.e castling + empassant)

--- board/chessboard.py
+++ board/chessboard.py
@@ -15,6 +15,9 @@ BLACK_PLAYER = 2
 
 
 class ChessBoard(Board):
+    ####################################################################################################################
+    ########################################## PUBLIC METHODS ##########################################################
+    ####################################################################################################################
     def __init__(self, **kwargs):
         super().__init__()
         self.debug = kwargs.get("debug", False)
@@ -39,54 +42,319 @@ class ChessBoard(Board):
             if i == 4:
                 board[0][i] = BLACK_DENOTER + KING
                 board[7][i] = WHITE_DENOTER + KING
-        reduced_state = (None, board, WHITE_PLAYER, True)
+        reduced_state = (None, board, WHITE_PLAYER, True, True, True, True)
         # Prev board, current board, player, can castle, legal_moves
         legal_moves = self.generate_legal_moves(reduced_state)
 
         return reduced_state + (legal_moves,)
 
     def get_state(self, state, action):
-        prev_board, curr_board, player, can_castle, legal_moves = state
+        prev_board, curr_board, player, p1ck, p1cq, p2ck, p2cq, legal_moves = state
 
         # check if action is in moves
-
         # make action, do appropriate edge checks (e.g pawn promotion)
+        if action not in legal_moves:
+            raise ValueError("Invalid move {}".format(action))
 
-
-
-################################################################
-        new_board = None
-        new_can_castle = can_castle
-################################################################
         new_prev = self.copy_board(curr_board)
-        new_player = self.get_other_player(player)
+        new_board = self.copy_board(curr_board)
+        r, c, e_r, e_c = action
+        new_board[e_r][e_c] = new_board[r][c]
+        new_board[r][c] = EMPTY_SQUARE
+        # TODO update castling!!
+        # TODO execute empassant (delete pawn above)
 
-        reduced_state = (new_prev, new_board, new_player, new_can_castle)
+        new_player = self.get_other_player(player)
+        reduced_state = (new_prev, new_board, new_player, p1ck, p1cq, p2ck, p2cq)
         legal_moves = self.generate_legal_moves(reduced_state)
 
         return reduced_state + (legal_moves,)
 
     def get_legal_moves(self, state):
-        _, _, _, _, legal_moves = state
+        _, _, _, _, _, _, _, legal_moves = state
         return legal_moves
 
     def ending_state(self, state):
-        raise NotImplementedError
+        _, curr_board, player, _, _, _, _, legal_moves = state
+        if legal_moves:
+            return 0
+        if self.king_in_check(curr_board, player):
+            return 1
+        else:
+            return -1
 
-    def get_other_player(self, player):
-        return BLACK_PLAYER if player == WHITE_PLAYER else WHITE_PLAYER
+    ####################################################################################################################
+    #################################### METHODS TO GET LEGAL MOVES ####################################################
+    ####################################################################################################################
+    def generate_legal_moves(self, reduced_state):
+        prev_board, curr_board, player, p1ck, p1cq, p2ck, p2cq = reduced_state
+        moves = []
+        for i in range(8):
+            for j in range(8):
+                p = curr_board[i][j]
+                if p == EMPTY_SQUARE or self.get_player_of_piece(curr_board, i, j) != player:
+                    continue
+                p = p[1]
+                if p == PAWN:
+                    moves += self.get_pawn_moves(curr_board, player, i, j, prev_board)
+                elif p == KNIGHT:
+                    moves += self.get_knight_moves(curr_board, player, i, j)
+                elif p == BISHOP:
+                    moves += self.get_bishop_moves(curr_board, player, i, j)
+                elif p == ROOK:
+                    moves += self.get_rook_moves(curr_board, player, i, j)
+                elif p == QUEEN:
+                    moves += self.get_queen_moves(curr_board, player, i, j)
+                elif p == KING:
+                    moves += self.get_king_moves(curr_board, player, i, j, p1ck, p1cq, p2ck, p2cq)
 
-    def can_move(self, reduced_state, s_row, s_col, e_row, e_col):
-        _, board, player, _ = reduced_state
-        if self.is_blocked(reduced_state, s_row, s_col, e_row, e_col):
+        return moves
+
+    def get_pawn_moves(self, board, player, row, col, prev_board):
+        pawn_moves = []
+        is_white = player == WHITE_PLAYER
+        can_double = row == 6 * is_white + 1 * (not is_white)
+
+        # check diag
+        for i in range(-1, 2, 2):
+            r = row + 1 + (-2 * is_white)
+            c = col + i
+            if self.in_bounds(r, c) and not self.will_cause_check(board, player, (row, col, r, c)):
+                    p = board[r][c]
+                    if (p != EMPTY_SQUARE and self.get_player_of_piece(board, r, c) != player) \
+                            or (self.is_empassant(prev_board, board, player, (row, col, r, c))):
+                        ## need to check empassant here
+                        pawn_moves.append((row, col, r, c))
+        # check single
+        r = row + 1 + (-2 * is_white)
+        if self.in_bounds(r, col) and not self.will_cause_check(board, player, (row, col, r, col)):
+            p = board[r][col]
+            if p == EMPTY_SQUARE:
+                pawn_moves.append((row, col, r, col))
+
+        # check_double
+        d_row = row + 2 + (-4 * is_white)
+        if can_double and not self.will_cause_check(board, player, (row, col, d_row, col)) \
+                and not self.is_blocked(board, player, (row, col, d_row, col)):
+            p = board[d_row][col]
+            if p == EMPTY_SQUARE:
+                pawn_moves.append((row, col, d_row, col))
+
+        return pawn_moves
+
+    def is_empassant(self, prev_board, board, player, move):
+        s_row, s_col, e_row, e_col = move
+        is_white = player == WHITE_PLAYER
+        if not s_row == 3 * is_white + 4 * (not is_white):
             return False
 
+        target_pawn = (BLACK_DENOTER if is_white else WHITE_DENOTER) + PAWN
+        prev_pawn = prev_board[e_row + 1 + (-2 * is_white)][e_col]
+        if prev_pawn != target_pawn:
+            return False
+
+        new_prev_pawn = board[e_row + 1 + (-2 * is_white)][e_col]
+        curr_pawn = board[e_row - 1 + (2 * is_white)][e_col]
+        if curr_pawn != target_pawn and new_prev_pawn != EMPTY_SQUARE:
+            return False
+        return True
+
+    def get_knight_moves(self, board, player, row, col):
+        knight_moves = []
+        for i in range(-2, 3, 4):
+            for j in range(-1, 2, 2):
+                e_row = row + i
+                e_col = col + j
+                if self.in_bounds(e_row, e_col) and not self.will_cause_check(board, player, (row, col, e_row, e_col)):
+                    p = board[e_row][e_col]
+                    if p == EMPTY_SQUARE or player != self.get_player_of_piece(board, e_row, e_col):
+                        knight_moves.append((row, col, e_row, e_col))
+                e_row = row + j
+                e_col = col + i
+                if self.in_bounds(e_row, e_col) and not self.will_cause_check(board, player, (row, col, e_row, e_col)):
+                    p = board[e_row][e_col]
+                    if p == EMPTY_SQUARE or player != self.get_player_of_piece(board, e_row, e_col):
+                        knight_moves.append((row, col, e_row, e_col))
+
+        return knight_moves
+
+    def get_bishop_moves(self, board, player, row, col):
+        bishop_moves = []
+        for i in range(-8, 9):
+            if i != 0:
+                # diag 'positive' slope
+                e_row, e_col = row + i, col - i
+                if self.in_bounds(e_row, e_col):
+                    if not self.is_blocked(board, player, (row, col, e_row, e_col)) \
+                            and not self.will_cause_check(board, player, (row, col, e_row, col)):
+                        bishop_moves.append((row, col, e_row, e_col))
+                # diag 'negative' slope
+                e_row, e_col = row + i, col + i
+                if self.in_bounds(e_row, e_col):
+                    if not self.is_blocked(board, player, (row, col, e_row, e_col)) \
+                            and not self.will_cause_check(board, player, (row, col, e_row, col)):
+                        bishop_moves.append((row, col, e_row, e_col))
+        return bishop_moves
+
+    def get_rook_moves(self, board, player, row, col):
+        rook_moves = []
+        for i in range(-8, 9):
+            if i != 0:
+                # horizontal
+                e_row, e_col = row, col + i
+                if self.in_bounds(e_row, e_col):
+                    if not self.is_blocked(board, player, (row, col, e_row, e_col)) \
+                            and not self.will_cause_check(board, player, (row, col, e_row, col)):
+                        rook_moves.append((row, col, e_row, e_col))
+                # vertical
+                e_row, e_col = row + i, col
+                if self.in_bounds(e_row, e_col):
+                    if not self.is_blocked(board, player, (row, col, e_row, e_col)) \
+                            and not self.will_cause_check(board, player, (row, col, e_row, col)):
+                        rook_moves.append((row, col, e_row, e_col))
+        return rook_moves
+
+    def get_queen_moves(self, board, player, row, col):
+        queen_moves = []
+        for i in range(-8, 9):
+            if i != 0:
+                # diag 'positive' slope
+                e_row, e_col = row + i, col - i
+                if self.in_bounds(e_row, e_col):
+                    if not self.is_blocked(board, player, (row, col, e_row, e_col)) \
+                            and not self.will_cause_check(board, player, (row, col, e_row, col)):
+                        queen_moves.append((row, col, e_row, e_col))
+                # diag 'negative' slope
+                e_row, e_col = row + i, col + i
+                if self.in_bounds(e_row, e_col):
+                    if not self.is_blocked(board, player, (row, col, e_row, e_col)) \
+                            and not self.will_cause_check(board, player, (row, col, e_row, col)):
+                        queen_moves.append((row, col, e_row, e_col))
+                # horizontal
+                e_row, e_col = row, col + i
+                if self.in_bounds(e_row, e_col):
+                    if not self.is_blocked(board, player, (row, col, e_row, e_col)) \
+                            and not self.will_cause_check(board, player, (row, col, e_row, col)):
+                        queen_moves.append((row, col, e_row, e_col))
+                # vertical
+                e_row, e_col = row + i, col
+                if self.in_bounds(e_row, e_col):
+                    if not self.is_blocked(board, player, (row, col, e_row, e_col)) \
+                            and not self.will_cause_check(board, player, (row, col, e_row, col)):
+                        queen_moves.append((row, col, e_row, e_col))
+        return queen_moves
+
+    def get_king_moves(self, board, player, row, col, p1ck, p1cq, p2ck, p2cq):
+        ## TODO Castle
+        king_moves = []
+        for i in range(-1, 2):
+            for j in range(-1, 2):
+                e_row, e_col = row + i, col + j
+                if not (i == 0 and j == 0) and self.in_bounds(e_row, e_col):
+                    if not self.is_blocked(board, player, (row, col, e_row, e_col)) \
+                            and self.will_cause_check(board, player, (row, col, e_row, e_col)):
+                        king_moves.append((row, col, e_row, e_col))
+        return king_moves
+
+    def copy_board(self, board):
+        return [[board[i][j] for j in range(8)] for i in range(8)]
+
+    ####################################################################################################################
+    ########################################### KING CHECK LOGIC #######################################################
+    ####################################################################################################################
+    def king_in_check(self, board, player):
+        pos = []
+        king_pos = None
+        for i in range(8):
+            for j in range(8):
+                p = board[i][j]
+                if p == EMPTY_SQUARE:
+                    continue
+                p_player = self.get_player_of_piece(board, i, j)
+                if p_player == player and p[1] == KING:
+                    king_pos = (i, j)
+                if p_player == self.get_other_player(player):
+                    pos.append((i, j))
+
+        for p in pos:
+            try:
+                if self.piece_can_move(board, self.get_other_player(player), (p + king_pos)):
+                    return True
+            except ValueError:
+                continue
+        return False
+
+    def will_cause_check(self, board, player, move):
+        s_row, s_col, e_row, e_col = move
         cb = self.copy_board(board)
         cb[e_row][e_col] = cb[s_row][s_col]
         cb[s_row][s_col] = EMPTY_SQUARE
         return self.king_in_check(cb, player)
 
-    def is_blocked(self, board, player, s_row, s_col, e_row, e_col):
+    def piece_can_move(self, board, player, move):
+        s_row, s_col, e_row, e_col = move
+        p = board[s_row][s_col]
+        if p == EMPTY_SQUARE or self.get_player_of_piece(board, s_row, s_col) != player:
+            raise ValueError("Piece must exist on ({}, {})".format(s_row, s_col))
+        p = p[1]
+        if p == PAWN:
+            return self.pawn_can_move(player, move)
+        elif p == KNIGHT:
+            return self.knight_can_move(move)
+        elif p == BISHOP:
+            return self.bishop_can_move(board, player, move)
+        elif p == ROOK:
+            return self.rook_can_move(board, player, move)
+        elif p == QUEEN:
+            return self.queen_can_move(board, player, move)
+        elif p == KING:
+            return self.king_can_move(move)
+        else:
+            raise ValueError("Invalid piece {}".format(p))
+
+    def pawn_can_move(self, player, move):
+        s_row, s_col, e_row, e_col = move
+        is_white = player == WHITE_PLAYER
+        target_moves = []
+        for i in range(-1, 2, 2):
+            r = s_row + 1 + (-2 * is_white)
+            c = s_col + i
+            if self.in_bounds(r, c):
+                target_moves.append((s_row, s_col, r, c))
+        if move in target_moves:
+            return True
+        return False
+
+    def knight_can_move(self, move):
+        s_row, s_col, e_row, e_col = move
+        return (abs(s_col - e_col) == 2 and abs(s_row - e_row) == 1) or (abs(s_row - e_row) == 2 and abs(s_col - e_col) == 1)
+
+    def bishop_can_move(self, board, player, move):
+        s_row, s_col, e_row, e_col = move
+        if abs(e_row - s_row) == abs(e_col - s_col):
+            if not self.is_blocked(board, player, move):
+                return True
+        return False
+
+    def rook_can_move(self, board, player, move):
+        s_row, s_col, e_row, e_col = move
+        if s_row == e_row or s_col == e_col:
+            if not self.is_blocked(board, player, move):
+                return True
+        return False
+
+    def queen_can_move(self, board, player, move):
+        return not self.is_blocked(board, player, move)
+
+    def king_can_move(self, move):
+        s_row, s_col, e_row, e_col = move
+        return abs(s_row - e_row) <= 1 and abs(s_col - e_col) <= 1
+    ####################################################################################################################
+    ############################################## AUX METHODS #########################################################
+    ####################################################################################################################
+
+    def is_blocked(self, board, player, move):
+        s_row, s_col, e_row, e_col = move
         squares = [(row, col) for (row, col) in self.generate_inbetween_squares(s_row, s_col, e_row, e_col)]
         for i in range(len(squares)):
             r, c = squares[i]
@@ -94,30 +362,6 @@ class ChessBoard(Board):
             if i == len(squares) - 1:
                 return p != EMPTY_SQUARE and not self.get_player_of_piece(board, r, c) == self.get_other_player(player)
             elif p != EMPTY_SQUARE:
-                return True
-        return False
-
-    def generate_legal_moves(self, reduced_state):
-        prev_board, curr_board, player, can_castle = reduced_state
-        return []
-
-    def copy_board(self, board):
-        return [[board[i][j] for j in range(8)] for i in range(8)]
-
-    def king_in_check(self, board, player):
-        pos = []
-        king_pos = None
-        for i in range(8):
-            for j in range(8):
-                p_player = self.get_player_of_piece(board, i, j)
-                p = board[i][j]
-                if p_player is player and p[1] is KING:
-                    king_pos = (i, j)
-                if p_player is self.get_other_player(player):
-                    pos.append((i, j))
-
-        for p in pos:
-            if not self.is_blocked(board, self.get_other_player(player), *p, *king_pos):
                 return True
         return False
 
@@ -160,8 +404,14 @@ class ChessBoard(Board):
             next_col += c_dir
             yield (next_row, next_col)
 
+    def get_other_player(self, player):
+        return BLACK_PLAYER if player == WHITE_PLAYER else WHITE_PLAYER
+
+    def in_bounds(self, row, col):
+        return row < 8 and row >= 0 and col < 8 and col >=0
+
     def print(self, state):
-        _, board, player, _, legal_moves = state
+        _, board, player, _, _, _, _, legal_moves = state
         print("Player {}'s turn.\n".format(player))
         if self.debug:
             print('  0  1  2  3  4  5  6  7')
@@ -195,7 +445,6 @@ class ChessBoard(Board):
 
         if self.debug:
             print('  0  1  2  3  4  5  6  7')
-            print('The player\'s legal moves are {}'.format(legal_moves))
         else:
             print('  a  b  c  d  e  f  g  h')
 
--- board/test_chess.py
+++ board/test_chess.py
@@ -1,65 +1,108 @@
 import unittest
-from board.chessboard import ChessBoard
+from board.chessboard import *
 
 
+chess_board = ChessBoard(debug=True)
 class TestTTT(unittest.TestCase):
     def test_init(self):
-        chess_board = ChessBoard()
-
         state = chess_board.get_initial_state()
         chess_board.print(state)
 
-    def test_copy(self):
-        chess_board = ChessBoard()
-        _, board, player, can_castle, legal_moves = chess_board.get_initial_state()
-
-        new_board = chess_board.copy_board(board)
-        new_board[5][5] = 'X'
-
-        self.assertEqual(new_board[5][5], 'X')
-        self.assertEqual(board[5][5], ' ')
-
-    def test_square_iteration(self):
-        chess_board = ChessBoard(debug=True)
-        for s in chess_board.generate_inbetween_squares(6, 4, 0, 4):
-            print(s)
-        print('\n')
-        for s in chess_board.generate_inbetween_squares(7, 7, 0, 0):
-            print(s)
-        print('\n')
-        for s in chess_board.generate_inbetween_squares(7, 0, 0, 7):
-            print(s)
-        print('\n')
-        for s in chess_board.generate_inbetween_squares(0, 0, 7, 7):
-            print(s)
-        print('\n')
-        for s in chess_board.generate_inbetween_squares(0, 7, 7, 0):
-            print(s)
-        print('\n')
-
-    def test_is_blocked(self):
-        def print_block(s_row, s_col, e_row, e_col):
-            print("Is blocked from ({}, {}) -> ({}, {})".format(s_row, s_col, e_row, e_col))
-        print("Testing is blocked...")
-        chess_board = ChessBoard(debug=True)
+    def test_make_moves(self):
         state = chess_board.get_initial_state()
-        prev_board, board, player, can_castle, _ = state
-        red_state = (prev_board, board, player, can_castle)
+        chess_board.print(state)
+        initial_moves = chess_board.get_legal_moves(state)
 
+        print("Starting moves")
+        print(initial_moves)
+
+    def test_king_in_check(self):
+        state = chess_board.get_initial_state()
+        prev_board, board, player, p1ck, p1cq, p2ck, p2cq, legal_moves = state
+        chess_board.print(state)
+        player = force_move(board, player, (6, 3, 4, 3))
+        self.assertFalse(chess_board.king_in_check(board, player))
+        player = force_move(board, player, (1, 3, 3, 3))
+        self.assertFalse(chess_board.king_in_check(board, player))
+        player = force_move(board, player, (6, 2, 4, 2))
+        self.assertFalse(chess_board.king_in_check(board, player))
+        player = force_move(board, player, (1, 4, 2, 4))
+        self.assertFalse(chess_board.king_in_check(board, player))
+        player = force_move(board, player, (6, 4, 4, 4))
+        self.assertFalse(chess_board.king_in_check(board, player))
+        player = force_move(board, player, (0, 5, 4, 1))
+        self.assertTrue(chess_board.king_in_check(board, player))
+        chess_board.print((None, board, player, True, True, True, True, []))
+        player = force_move(board, player, (7, 3, 6, 3))
+        self.assertFalse(chess_board.king_in_check(board, player))
+        chess_board.print((None, board, player, True, True, True, True, []))
+        player = force_move(board, player, (0, 3, 3, 3))
+        self.assertFalse(chess_board.king_in_check(board, player))
+
+    def test_get_moves(self):
+        state = chess_board.get_initial_state()
+        prev_board, board, player, p1ck, p1cq, p2ck, p2cq, legal_moves = state
+        board[3][3] = WHITE_DENOTER + BISHOP
+        chess_board.print(state)
+        print(chess_board.get_bishop_moves(board, player, 3, 3))
+
+    def test_playout(self):
+        print("Testing self_play")
+        state = chess_board.get_initial_state()
+        move_history = []
+        try:
+            while not chess_board.ending_state(state):
+                print("\n\nGame state\n\n")
+                chess_board.print(state)
+                print("\n\nMove sequence: \n\n{}".format(move_history))
+                legal_moves = chess_board.get_legal_moves(state)
+                print("Make move:\n{}".format(legal_moves))
+                move = input("Move?")
+                move = list(move)
+                move = tuple(list(filter(check_char, move)))
+                move = tuple(map(int, move))
+                while move not in legal_moves:
+                    move = input("Move? that was wrong.")
+                    move = tuple(list(filter(check_char, list(move))))
+                    move = tuple(map(int, move))
+                move_history.append(move)
+                state = chess_board.get_state(state, move)
+        except ValueError:
+            print("Exception occurred. Move history:\n".format(move_history))
+
+    def test_empassant(self):
+        print("Test Empassant")
+        state = chess_board.get_initial_state()
+        move_seq = ((6, 4, 4, 4), (1, 4, 3, 4), (6, 3, 4, 3), (3, 4, 4, 3))
+        state = recreate_state(state, move_seq)
+        state = chess_board.get_state(state, (6, 2, 4, 2))
+        chess_board.print(state)
+        state = chess_board.get_state(state, (1, 0, 2, 0))
+        chess_board.print(state)
+        state = chess_board.get_state(state, (6, 0, 5, 0))
+        chess_board.print(state)
+        state = chess_board.get_state(state, (4, 3, 5, 2))
         chess_board.print(state)
 
-        row_col = (6, 4, 1, 4)
-        print_block(*row_col)
-        self.assertFalse(chess_board.is_blocked(red_state, *row_col))
 
-        row_col = (6, 4, 0, 4)
-        print_block(*row_col)
-        self.assertTrue(chess_board.is_blocked(red_state, *row_col))
 
+def recreate_state(state, move_sequence):
+    for move in move_sequence:
+        state = chess_board.get_state(state, move)
+    return state
 
 
 
+def check_char(c):
+    try:
+        int(c)
+        return True
+    except ValueError:
+        return False
 
 
-
-
+def force_move(board, player, move):
+    s_row, s_col, e_row, e_col = move
+    board[e_row][e_col] = board[s_row][s_col]
+    board[s_row][s_col] = EMPTY_SQUARE
+    return chess_board.get_other_player(player)
