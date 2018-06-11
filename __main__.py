d39c621cf6a416062799438bd854d24a45dc0fa7
Alex Jackson <ajjackson91195@gmail.com>
2018-06-10 21:17:02 -0700

    Add ability for chess self play

--- __main__.py
+++ __main__.py
@@ -1,4 +1,5 @@
 from game.ttt_game import TTTGame
+from game.chess_game import ChessGame
 import argparse
 
 
@@ -8,13 +9,26 @@ def main():
                         help='thinking time for ai')
     parser.add_argument("-p", "--player", default=2, type=int, nargs='?',
                         help='human player (1 or 2)')
+    parser.add_argument("-g", "--game", default='ttt', type=str, nargs='?',
+                        help='which game to play (chess or ttt)')
+    parser.add_argument("-d", "--debug", default=False, type=bool, nargs='?',
+                        help='enable debug mode')
+    parser.add_argument("-x", "--self_play", default=False, type=bool, nargs='?',
+                        help='enable self play mode')
 
     args = parser.parse_args()
     searchtime = args.searchtime
     player = args.player
+    game_type = args.game
+    debug = args.debug
+    self_play = args.self_play
     # use parameters player=%1|2 and searchtime=%seconds
     # TTTGame(player=1, searchtime=10)
-    game = TTTGame(searchtime=searchtime, player=player)
+    if game_type == 'ttt':
+        game = TTTGame(searchtime=searchtime, player=player)
+    elif game_type == 'chess':
+        game = ChessGame(searchtime=searchtime, player=player, debug=debug, self_play=self_play)
+
     game.start()
 
 
--- game/chess_game.py
+++ game/chess_game.py
@@ -1,15 +1,45 @@
 from board.chessboard import ChessBoard
 from mctsai.mcts import MCTS
+from board.test_chess import check_char
 
 class ChessGame():
     def __init__(self, **kwargs):
         self.search_time = kwargs.get("searchtime", 10)
         self.player = kwargs.get("player", 1)
         self.debug = kwargs.get("debug", False)
+        self.self_play = kwargs.get("self_play", True)
 
     def start(self):
-        chess = ChessBoard(debug=self.debug)
-        mcts = MCTS(chess, searchtime=self.search_time)
-        state = chess.get_initial_state()
-        # Implement turn logic here
-        chess.print(state)
+        if self.self_play:
+            self.play_self()
+        else:
+            chess = ChessBoard(debug=self.debug)
+            mcts = MCTS(chess, searchtime=self.search_time)
+            state = chess.get_initial_state()
+            # Implement turn logic here
+            chess.print(state)
+
+    def play_self(self):
+        chess_board = ChessBoard(debug=self.debug)
+        print("Testing self_play")
+        state = chess_board.get_initial_state()
+        move_history = []
+        try:
+            while not chess_board.ending_state(state):
+                print("\n\nGame state\n\n")
+                chess_board.print(state)
+                print("\n\nMove sequence: \n\n{}".format(move_history))
+                legal_moves = chess_board.get_legal_moves(state)
+                print("Make move:\n{}".format(legal_moves))
+                move = input("Move?")
+                move = list(move)
+                move = tuple(list(filter(check_char, move)))
+                move = tuple(map(int, move))
+                while move not in legal_moves:
+                    move = input("Move? that was wrong.")
+                    move = tuple(list(filter(check_char, list(move))))
+                    move = tuple(map(int, move))
+                move_history.append(move)
+                state = chess_board.get_state(state, move)
+        except ValueError:
+            print("Exception occurred. Move history:\n".format(move_history))
